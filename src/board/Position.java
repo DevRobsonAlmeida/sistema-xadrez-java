7599c0d5efdd5c7eaf0b59478c453573a1463104
RobsonAlmeida <robsonalmeida073@gmail.com>
2021-07-30 15:09:04 -0300

    Imprimindo o tabuleiro

---
+++ src/board/Position.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package board;
+
+/**
+ *
+ * @author 99030499
+ */
+public class Position {
+    private int row;
+    private int column;
+
+    public Position() {
+    }
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    
+    public void setValues(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+
+    
+    
+}
